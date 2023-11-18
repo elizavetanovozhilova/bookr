2b321f42c032488169baa1cbacc63375b3414023
elizavetanovozilova <babygorilla@yandex.ru>
2023-11-18 17:46:14 +0300

    update

--- bookr/urls.py
+++ bookr/urls.py
@@ -14,18 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
 import reviews.views
+from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', reviews.views.index),
-    path('book_search/', reviews.views.book_search)
+    path("admin/", admin.site.urls),
+    path("", reviews.views.index),
+    path("book_search/", reviews.views.book_search, name="book_search"),
+    path("", include("reviews.urls")),
 ]
-from django.urls import include, path
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('book-search/', reviews.views.book_search, name='book_search'),
-    path('', include('reviews.urls'))
-    ]
+
